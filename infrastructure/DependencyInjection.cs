2245715a822c7696bc5923edf5c0a2934c7f165a
Vishnu Kumar PS <vishnu.kumarps@claysys.net>
2023-11-05 04:22:21 +0530

    Add project files.

---
+++ infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+    }
+}
