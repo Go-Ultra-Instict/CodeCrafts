8ed1600fe4b2ff622dfd4646d95ac75155c50c46
Vishnu Kumar PS <vishnu.kumarps@claysys.net>
2023-11-12 10:51:58 +0530

    automapper first try

--- Application/StudentTopic/Queries/GetStudents/GetStudentQuery.cs
+++ Application/StudentTopic/Queries/GetStudents/GetStudentQuery.cs
@@ -1,12 +1,10 @@
 ﻿using Application.Interfaces;
-using Application.StudentTopic.Commands.CreateStudents;
-using AutoMapper.QueryableExtensions;
+using AutoMapper;
 using CodeCrafts.Domain.Entities;
 using CodeCrafts.Domain.Exceptions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System.Net;
-using System.Web.Http;
+
 
 namespace Application.StudentTopic.Queries.GetStudents;
 
@@ -17,9 +15,11 @@ public class GetStudentQuery : IRequest<StudentDto>
 
 public class GetStudentQueryHandler : IRequestHandler<GetStudentQuery, StudentDto>
 {
-    public GetStudentQueryHandler(IDemoDbContext dbContext)
+    private readonly IMapper _mapper;
+    public GetStudentQueryHandler(IDemoDbContext dbContext, IMapper mapper)
     {
         _dbContext = dbContext;
+        _mapper = mapper;
     }
 
     public IDemoDbContext _dbContext { get; }
@@ -27,20 +27,32 @@ public class GetStudentQueryHandler : IRequestHandler<GetStudentQuery, StudentDt
     public async Task<StudentDto> Handle(GetStudentQuery request, CancellationToken cancellationToken)
     {
 
-        //throw new NotFoundCustomException(nameof(Student),"doest has this id");
-        //throw new NotImplementedException("TEST");
-
-        var student = await _dbContext.Students.Where(x => x.Id == request.Id).Select(x => new StudentDto
-        {
-            Id = x.Id,
-            Name = x.Name,
-        }).FirstOrDefaultAsync(cancellationToken);
+        Student? student = await _dbContext.Students.Where(x => x.Id == request.Id).FirstOrDefaultAsync(cancellationToken);
 
         if (student == null)
         {
             throw new NotFoundCustomException("Item not found");
         }
 
-        return student;
+        StudentDto? dest = _mapper.Map<Student, StudentDto>(student);
+
+        //Test auto mapper
+        Student? backtoStudent= _mapper.Map<StudentDto,Student>(dest);
+        List<StudentDto>? destList1 = _mapper.Map<List<Student>, List<StudentDto>>( new List<Student> { student});
+        List<Student>? destList2 = _mapper.Map<List<StudentDto>, List<Student>>(new List<StudentDto> { dest });
+
+        return dest;
     }
 }
+
+/***
+ * old code
+ *   //throw new NotFoundCustomException(nameof(Student),"doest has this id");
+        //throw new NotImplementedException("TEST");
+
+        //var student = await _dbContext.Students.Where(x => x.Id == request.Id).Select(x => new StudentDto
+        //{
+        //    Id = x.Id,
+        //    Name2 = x.Name,
+        //}).FirstOrDefaultAsync(cancellationToken);
+ */
